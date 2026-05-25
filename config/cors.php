d1dc6dea393ecdde8a8f0c3c15573fafa82ff157
carlosdcastano <carlosdueiner1996@gmail.com>
2026-05-25 18:00:36 -0500

    Cors y google oauth finalizados

---
+++ config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'paths'                    => ['api/*'],
+    'allowed_methods'          => ['*'],
+    'allowed_origins'          => [
+        env('FRONTEND_NEBULA_URL',  'http://localhost:3000'),
+        env('FRONTEND_ADMIN_URL',   'http://localhost:3001'),
+        env('FRONTEND_TICKETS_URL', 'http://localhost:3002'),
+        env('FRONTEND_ACCESS_URL',  'http://localhost:3003'),
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers'          => ['*'],
+    'exposed_headers'          => [],
+    'max_age'                  => 0,
+    'supports_credentials'     => true,
+];
