63f101974a81869ce08bdc1b5ab0001416d6cf17
carlosdcastano <carlosdueiner1996@gmail.com>
2026-05-26 17:26:45 -0500

    Local test

--- config/cors.php
+++ config/cors.php
@@ -11,6 +11,7 @@ return [
             'http://127.0.0.1:5502',  // desarrollo local
             'http://localhost:5502',
             'http://127.0.0.1:5500',   // desarrollo local
+            'http://127.0.0.1:5501',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
